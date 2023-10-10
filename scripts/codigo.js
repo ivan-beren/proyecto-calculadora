fda4d63856cecfc9568ca7789f65352e6ce91e9a
Ivan Berengeno <ivan.beren.mdp@gmail.com>
2023-10-09 22:58:33 -0300

    Se agregan divs en el html y se empieza a manipular el css

---
+++ scripts/codigo.js
@@ -0,0 +1,15 @@
+//let numero1 = parseInt(prompt(`Indique el primer numero`));
+
+//let numero2 = parseInt(prompt(`Indique el segundo numero`));
+
+let resultado = 0;
+
+if(!isNaN(numero1) && !isNaN(numero2)){
+    resultado = numero1 + numero2;
+    alert(resultado)
+};
+
+
+
+
+
