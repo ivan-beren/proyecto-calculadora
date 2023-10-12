1a3eed4801553393888d78c20293a6b5210acc82
Ivan Berengeno <ivan.beren.mdp@gmail.com>
2023-10-12 19:05:16 -0300

    Se agrega la logica utilizando el operador switch

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -1,14 +1,34 @@
-//let numero1 = parseInt(prompt(`Indique el primer numero`));
+let numero1 = parseInt(prompt(`Indique el primer numero`));
 
-//let numero2 = parseInt(prompt(`Indique el segundo numero`));
+let signo = prompt(`Indique el signo`)
+
+let numero2 = parseInt(prompt(`Indique el segundo numero`));
 
 let resultado = 0;
 
-if(!isNaN(numero1) && !isNaN(numero2)){
-    resultado = numero1 + numero2;
-    alert(resultado)
-};
+switch(signo){
+    case "+":
+        resultado = numero1 + numero2;
+        console.log("Se suman los dos numeros")
+        break;
+    case "-":
+        resultado = numero1 - numero2;
+        console.log("Se restan los dos numeros");
+        break;
+    case "*":
+        resultado = numero1 * numero2;
+        console.log("Se multiplican los dos numeros");
+        break;
+    case "/":
+        resultado = numero1 / numero2;
+        console.log("Se divide el primer numero por el segundo");
+        break;
+    default:
+        console.log("Operacion no valida");
+        break;
+}
 
+console.log(`El resultado es ${resultado}`)
 
 
 
