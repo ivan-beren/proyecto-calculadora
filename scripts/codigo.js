df45c1396508b8a801e3571b759a96b11ff934fb
Ivan Berengeno <ivan.beren.mdp@gmail.com>
2023-10-15 20:43:16 -0300

    Se agregan las etiquetas button e input al html

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -1,10 +1,35 @@
-let numero1 = parseInt(prompt(`Indique el primer numero`));
+//let numero1 = parseInt(prompt(`Indique el primer numero`));
 
-let signo = prompt(`Indique el signo`)
+//let signo = prompt(`Indique el signo`)
 
-let numero2 = parseInt(prompt(`Indique el segundo numero`));
+//let numero2 = parseInt(prompt(`Indique el segundo numero`));
 
 let resultado = 0;
+
+
+let numeroPantalla = document.getElementById("numeroPantalla");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btnDiv = document.getElementById("btnDiv");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btnMult = document.getElementById("btnMult");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btnResta = document.getElementById("btnResta");
+let btn0 = document.getElementById("btn0");
+let btnComa = document.getElementById("btnComa");
+let btnIgual = document.getElementById("btnIgual");
+let btnSuma = document.getElementById("btnSuma");
+
+
+
+btn1.addEventListener('click', () =>{
+    alert("Soy un boton")
+})
 
 switch(signo){
     case "+":
